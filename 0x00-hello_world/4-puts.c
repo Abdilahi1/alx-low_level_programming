60760be037aaca926ffacf175445d510c2440aae
Abdilahi1 <abdallejibril333@gmail.com>
2023-03-19 00:32:14 -0700

    my first commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main  - Entery point
+ *
+ * Describtion of the file
+ *
+ * puts file
+ * */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
